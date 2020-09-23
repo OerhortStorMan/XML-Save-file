60a8e30ac6e4e8abb0d810c0281694277280e2be
lovewinelid <love.winelid@elev.ga.ntig.se>
2020-09-23 11:09:32 +0200

    Inlämn1

--- XML Save file/Program.cs
+++ XML Save file/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace XML_Save_file
 {
     class Program
     {
+        static XmlSerializer serializer = new XmlSerializer(typeof(Savefile));
+        static FileStream myFile = File.Open("Savefile.xml", FileMode.OpenOrCreate);
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Välkommen till 'skapa din egna kock'-spelet.");
+
+            bool correctInput = false;
+            while (!correctInput)
+            {
+                System.Console.WriteLine("Vill du [A] skapa en ny save file eller [B] öppna en befintlig?");
+                string createOrOpenAnswer = Console.ReadLine();
+
+                createOrOpenAnswer = createOrOpenAnswer.ToLower();
+
+                if (createOrOpenAnswer == "a")
+                {
+                    CharacterCreator();
+                    OpenCharacter();
+                    correctInput = true;
+                }
+                else if (createOrOpenAnswer == "b")
+                {
+                    OpenCharacter();
+                    correctInput = true;
+
+                }
+                else
+                {
+                    System.Console.WriteLine("Ge ett giltigt svar, tack.");
+                }
+            }
+        }
+
+        static void CharacterCreator()
+        {
+            Console.Clear();
+            Savefile player1 = new Savefile();
+
+            System.Console.WriteLine("Vad ska din karaktär heta?");
+            player1.name = Console.ReadLine();
+            
+            System.Console.WriteLine("Hur mycket movement speed?");
+            string movementInput = Console.ReadLine();
+            bool success = int.TryParse(movementInput, out int movementInt)
+            player1.movementSpeed = movementInt;
+
+
+
+
+
+            serializer.Serialize(myFile, player1);
+
+            
+        }
+
+        static void OpenCharacter()
+        {
+            //player1 = (Savefile) serializer.Deserialize(myFile);
         }
     }
 }
---
+++ XML Save file/Savefile.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace XML_Save_file
+{
+    public class Savefile
+    {
+        public string name = "x";
+        public int movementSpeed;
+
+        public int skillpower;
+    }
+}
