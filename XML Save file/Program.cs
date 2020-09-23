ed795b1fcbd596fd14e632763eb652eaedf9a285
lovewinelid <love.winelid@elev.ga.ntig.se>
2020-09-23 10:03:54 +0200

    Init.

---
+++ XML Save file/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XML_Save_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
